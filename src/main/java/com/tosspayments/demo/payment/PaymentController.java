84ebab0fff7cb9c2a43c4dda187cafbfec3239d1
yhwit30 <yhwit30@gmail.com>
2024-04-09 15:08:10 +0900

    토스 결제 테스트

--- src/main/java/com/tosspayments/demo/payment/PaymentController.java
+++ src/main/java/com/tosspayments/demo/payment/PaymentController.java
@@ -26,7 +26,7 @@ public class PaymentController {
             @RequestParam(value = "amount") Integer amount,
             @RequestParam(value = "paymentKey") String paymentKey) throws Exception {
 
-        String secretKey = "test_sk_zXLkKEypNArWmo50nX3lmeaxYG5R:";
+        String secretKey = "test_sk_ex6BJGQOVDJAXJNKXwdJ8W4w2zNb:";
 
         Base64.Encoder encoder = Base64.getEncoder();
         byte[] encodedBytes = encoder.encode(secretKey.getBytes("UTF-8"));
